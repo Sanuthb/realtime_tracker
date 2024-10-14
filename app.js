bc354471aafe4fbfe3f7a0025775d4c26699054d
Sanuthb <165442353+Sanuthb@users.noreply.github.com>
2024-10-14 06:57:11 +0530

    tracker

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const http = require('http');
+const path = require('path');
+
+const app = express()
+const socketio = require('socket.io');
+const server=http.createServer(app)
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+
+io.on('connection', (socket) => {
+    socket.on('send-location',function(data){
+        io.emit('receive-location',{id:socket.id, ...data});
+    })
+    socket.on('disconnect',function(data){
+        io.emit('user-disconnected',socket.id);
+    })
+    console.log('connection')
+})
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+server.listen(9000)
